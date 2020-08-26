83a2a3e5b97e5a48e0d9270678bcb4ae9a46fc70
Karn Vadaliya <vadaliya.k@husky.neu.edu>
2020-08-26 04:46:53 -0400

    Deploy

---
+++ build/precache-manifest.3750514be446750b75b02fd1237816f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49bd0a589ac9d9412fd2bffb478b6fe",
+    "url": "/WeatherForecast/index.html"
+  },
+  {
+    "revision": "c47369191e9367f299e0",
+    "url": "/WeatherForecast/static/css/main.941b42a3.chunk.css"
+  },
+  {
+    "revision": "9068bc5fae655a57ea55",
+    "url": "/WeatherForecast/static/js/2.d526331f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/WeatherForecast/static/js/2.d526331f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c47369191e9367f299e0",
+    "url": "/WeatherForecast/static/js/main.05abbc0f.chunk.js"
+  },
+  {
+    "revision": "c7d944c23ae19a715527",
+    "url": "/WeatherForecast/static/js/runtime-main.ebccad36.js"
+  },
+  {
+    "revision": "bf33a3798c6289a4410490bd00ea1221",
+    "url": "/WeatherForecast/static/media/backgrd.bf33a379.jpg"
+  },
+  {
+    "revision": "f0cd153053acebd3ad30bcd3cf0d480e",
+    "url": "/WeatherForecast/static/media/current2.f0cd1530.jpg"
+  }
+]);
