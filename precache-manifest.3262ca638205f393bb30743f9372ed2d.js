a565683918577b818e9b1e7d479744644fa6437d
Karn Vadaliya <karn.vadaliya@gmail.com>
2021-08-23 00:23:38 -0400

    Updates

---
+++ precache-manifest.3262ca638205f393bb30743f9372ed2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4dfa956c28210182e5a358aafa9772b",
+    "url": "/WeatherForecast/index.html"
+  },
+  {
+    "revision": "0d73c33d28d323b72eb5",
+    "url": "/WeatherForecast/static/css/main.941b42a3.chunk.css"
+  },
+  {
+    "revision": "5af73751cc5cb6ccaab0",
+    "url": "/WeatherForecast/static/js/2.52d40122.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/WeatherForecast/static/js/2.52d40122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d73c33d28d323b72eb5",
+    "url": "/WeatherForecast/static/js/main.2c5e98b5.chunk.js"
+  },
+  {
+    "revision": "c7d944c23ae19a715527",
+    "url": "/WeatherForecast/static/js/runtime-main.ebccad36.js"
+  },
+  {
+    "revision": "bf33a3798c6289a4410490bd00ea1221",
+    "url": "/WeatherForecast/static/media/backgrd.bf33a379.jpg"
+  },
+  {
+    "revision": "f0cd153053acebd3ad30bcd3cf0d480e",
+    "url": "/WeatherForecast/static/media/current2.f0cd1530.jpg"
+  }
+]);
